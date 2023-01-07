d791444eb7ed538fd3d184a9ec904e7382a44294
Fabio Scaparro <fabiovt_99@hotmail.it>
2023-01-07 12:50:31 +0100

    added opencv ransac for comparison

--- sfm1b/eight_point_ransac.py
+++ sfm1b/eight_point_ransac.py
@@ -8,7 +8,7 @@ def ransac(p1: np.ndarray,
            p2: np.ndarray, 
            threshold=5e-3, # in/outlier gating threshold
            prob=0.99, # to extract number of iterations
-           max_iters=100000,
+           max_iters=100,
            refine=False
         ):     
         assert p1.shape == p2.shape
@@ -23,7 +23,6 @@ def ransac(p1: np.ndarray,
         # math.ceil(math.log(1-prob) / math.log(1-w**5))
         # so I added another parameter to cap the iterations
         n_iters = min(n_iters, max_iters)
-        n_iters = 500000
         # best model 
         E = None
         best_inliers = []
@@ -68,3 +67,27 @@ def _eval_model(error, inliers):
     # define a function to evaluate a proposal
     # naive approach: count inliers
     return len(inliers)
+
+# wrapper around opencv implementation
+import cv2
+def ransac_opencv(p1: np.ndarray, 
+           p2: np.ndarray, 
+           threshold=5e-3, # in/outlier gating threshold
+           prob=0.99, # to extract number of iterations
+           max_iters=100,
+           ):
+
+    # cv2 wants 2D points, divide by z
+    p1_norm = p1 / np.vstack((p1[:,2],p1[:,2],p1[:,2])).T
+    p2_norm = p2 / np.vstack((p2[:,2],p2[:,2],p1[:,2])).T
+    # cv2 returns the transposed with respect to my convention
+    # so I swap the inputs
+    E, _inliers = cv2.findEssentialMat(p2_norm[:,:2],p1_norm[:,:2], 
+                                      method=cv2.RANSAC, threshold=threshold, 
+                                      prob=prob, maxIters=max_iters)
+    # cv2 returns a boolean array mask, extract the indexes from that
+    idxes = np.arange(p1.shape[0])
+    inliers = idxes[_inliers]
+
+    return E, inliers
+
